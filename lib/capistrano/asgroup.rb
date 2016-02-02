2369ffa7c3e70b1160875190f3de726ae88881b9
teu <pjasiulewicz@gmail.com>
2016-02-02 13:31:58 +0100

    stuff is working now

--- lib/capistrano/asgroup.rb
+++ lib/capistrano/asgroup.rb
@@ -17,22 +17,23 @@ module Capistrano
 
              # Get descriptions of all the Auto Scaling groups
             @autoScaleDesc = @as_api.describe_auto_scaling_groups
-             # Get descriptions of all the EC2 instances
-            @ec2DescInst = @ec2_api.describe_instances
 
+            @asGroupInstanceIds = Array.new()
             # Find the right Auto Scaling group
             @autoScaleDesc[:auto_scaling_groups].each do |asGroup|
-                @asGroupInstanceIds = Array.new
                   # Look for an exact name match or Cloud Formation style match (<cloud_formation_script>-<as_name>-<generated_id>)
                 if asGroup[:auto_scaling_group_name] == which or asGroup[:auto_scaling_group_name].scan("{which}").length > 0
                     # For each instance in the Auto Scale group
                     asGroup[:instances].each do |asInstance|
-                       @asGroupInstanceIds.push(asInstance[:instance_id])
+                        @asGroupInstanceIds.push(asInstance[:instance_id])
                     end
                 end
             end
 
-            # figure out the instance IP's
+             # Get descriptions of all the EC2 instances
+            @ec2DescInst = @ec2_api.describe_instances(instance_ids: @asGroupInstanceIds)
+
+             # figure out the instance IP's
             @ec2DescInst[:reservations].each do |reservation|
                 #remove instances that are either not in this asGroup or not in the "running" state
                 reservation[:instances].delete_if{ |a| not @asGroupInstanceIds.include?(a[:instance_id]) or a[:state][:name] != "running" }.each do |instance|
