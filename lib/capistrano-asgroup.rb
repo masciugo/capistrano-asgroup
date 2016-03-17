7ba6971d6e3348424ce53660895d42eb9d89b9b6
masciugo <masciugo@gmail.com>
2016-03-17 13:31:12 +0100

    fix gem load path

--- lib/capistrano-asgroup.rb
+++ lib/capistrano-asgroup.rb
@@ -1,8 +1,7 @@
 require 'capistrano/asgroup'
 
-
 if Gem::Specification.find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
-  load File.expand_path('capistrano/asgroup.rb', __FILE__)
+  load File.expand_path('capistrano/asgroup.rb', File.dirname(__FILE__))
 else
   puts "Oops, you may need capistrano 3+"
 end
